ee8c136069aa3375b56382cd8dfcb3146a282cb6
kimjaeyoung15 <151435248+kimjaeyoung15@users.noreply.github.com>
2023-11-26 22:29:47 +0900

    Add files via upload

--- LinearRegression_WARAnalysis.R
+++ LinearRegression_WARAnalysis.R
@@ -11,9 +11,12 @@ library(lmtest)
 
 #working directory
 setwd('C:\\Users\\Finance_Analytics\\WARAnalysis')
+setwd('C:\\Users\\esteb\\Documents\\GitHub\\WARAnalysis-KBO2023\\WARAnalysis_Cleaned_v0.2')
 Dataset_WARAnalysis_v0.1 <- read.csv('Dataset_WARAnalysis_v0.1.csv')
 
 #prototype models
+
+#model 0.1 series
 
 LM_Prototype_WARAnalysis_v0.1.1 <- lm(war~batting_average+on_base_percentage+slugging_percentage, data=Dataset_WARAnalysis_v0.1)
 summary(LM_Prototype_WARAnalysis_v0.1.1)
@@ -62,3 +65,58 @@ kurtosis(LM_Cleaned_WARAnalysis_v0.1.1$residuals)
 saveRDS(LM_Cleaned_WARAnalysis_v0.1.1, file='C:\\Users\\Finance_Analytics\\WARAnalysis\\LM_Cleaned_WARAnalysis_v0.1.1.rds')
 #significant, and residual plot's variance is smaller than LM_Prototype_WARAnalysis_v0.1.5
 
+#cleaned dataset 0.2
+
+#model 0.2.1 series(contain all variables)
+Dataset_Cleaned_WARAnalysis_v0.2 <- read.csv('Dataset_Cleaned_WARAnalysis_v0.2.csv')
+LM_Cleaned_WARAnalysis_v0.2.1 <- lm(war~hits_game+doubles_game+triples_game+homeruns_game+rbi_game
+                                    +stolen_game+caught_game+bb_game+hp_game, 
+                                    data=Dataset_Cleaned_WARAnalysis_v0.2) #basehits_game is removed because of high multicolinearity
+LM_Cleaned_WARAnalysis_v0.2.1l <- lm(ln_war~ln_hits_game+ln_doubles_game+ln_triples_game+ln_homeruns_game
+                                     +ln_basehits_game+ln_rbi_game+ln_stolen_game+ln_caught_game+ln_bb_game+ln_hp_game, 
+                                     data=Dataset_Cleaned_WARAnalysis_v0.2)
+summary(LM_Cleaned_WARAnalysis_v0.2.1)
+summary(LM_Cleaned_WARAnalysis_v0.2.1l)
+vif(LM_Cleaned_WARAnalysis_v0.2.1)
+vif(LM_Cleaned_WARAnalysis_v0.2.1l)
+step(LM_Cleaned_WARAnalysis_v0.2.1, direction = 'both')
+summary(lm(formula = war ~ hits_game + doubles_game + homeruns_game + 
+             stolen_game + bb_game, data = Dataset_Cleaned_WARAnalysis_v0.2))
+step(LM_Cleaned_WARAnalysis_v0.2.1l, direction = 'both')
+summary(lm(formula = ln_war ~ ln_hits_game + ln_basehits_game + ln_bb_game, 
+           data = Dataset_Cleaned_WARAnalysis_v0.2))
+
+#model 0.2.2 series(selected variables)
+
+#model 0.2.2a - variables are selected by step method
+LM_Cleaned_WARAnalysis_v0.2.2a <- lm(formula = war ~ hits_game + homeruns_game + 
+                                       stolen_game + bb_game, data = Dataset_Cleaned_WARAnalysis_v0.2)
+LM_Cleaned_WARAnalysis_v0.2.2la <- lm(formula = ln_war ~ ln_hits_game + ln_basehits_game + ln_bb_game, 
+                                     data = Dataset_Cleaned_WARAnalysis_v0.2)
+summary(LM_Cleaned_WARAnalysis_v0.2.2a)
+summary(LM_Cleaned_WARAnalysis_v0.2.2la)
+par(mfrow=c(2,3))
+plot(LM_Cleaned_WARAnalysis_v0.2.2a, which=c(1:6))
+plot(LM_Cleaned_WARAnalysis_v0.2.2la, which=c(1:6))
+shapiro.test(LM_Cleaned_WARAnalysis_v0.2.2a$residuals)
+
+
+#model 0.2.2b
+LM_Cleaned_WARAnalysis_v0.2.3 <- lm(war~., data=Dataset_Cleaned_WARAnalysis_v0.2)
+step(LM_Cleaned_WARAnalysis_v0.2.3, direction = 'both')
+LM_Cleaned_WARAnalysis_v0.2.3a <- lm(formula = war ~ ln_hits_game + ln_triples_game + ln_homeruns_game + ln_basehits_game
+                                     + ln_bb_game+ln_stolen_game+ln_caught_game+ln_hp_game, data = Dataset_Cleaned_WARAnalysis_v0.2)
+summary(LM_Cleaned_WARAnalysis_v0.2.3a)
+summary(lm(war~hits_game+doubles_game+triples_game+homeruns_game, 
+           data=Dataset_Cleaned_WARAnalysis_v0.2))
+LM_Cleaned_WARAnalysis_v0.2.3la <- lm(ln_war~hits_game+doubles_game+triples_game+homeruns_game+rbi_game
+                                      +stolen_game+caught_game+bb_game+hp_game, 
+                                      data=Dataset_Cleaned_WARAnalysis_v0.2)
+summary(LM_Cleaned_WARAnalysis_v0.2.3la)
+step(LM_Cleaned_WARAnalysis_v0.2.3la, direction = 'both')
+LM_Cleaned_WARAnalysis_v0.2.3lb <- lm(formula = ln_war ~ hits_game + homeruns_game + 
+                                        stolen_game + bb_game, data = Dataset_Cleaned_WARAnalysis_v0.2)
+summary(LM_Cleaned_WARAnalysis_v0.2.3lb)
+plot(LM_Cleaned_WARAnalysis_v0.2.3lb, which=c(1:6))
+write.csv(Dataset_Cleaned_WARAnalysis_v0.2, file='C:\\Users\\esteb\\Documents\\GitHub\\WARAnalysis-KBO2023\\WARAnalysis_Cleaned_v0.2\\Dataset_Cleaned_WARAnalysis_v0.2.csv')
+?write.csv
